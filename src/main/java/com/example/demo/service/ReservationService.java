1b2c733ea59b888cd355d6fa12c0de05c76fae6d
taketheking <so4524@gmail.com>
2024-12-13 20:01:20 +0900

    Fix(function_1) : 예외 발생시 reservation 과 rentalLog 모두 저장되거나 모두 저장되지 않도록 수정하여 오류 수정
    기존에는 rentalLog에서 예외가 발생해도 reservation은 등록이 되는 문제가 있었다. 이것을 @Transactional 로 모두 저장되거 모두 저장되지 않도록 하여 데이터베이스의 원자성을 지켰다.

--- src/main/java/com/example/demo/service/ReservationService.java
+++ src/main/java/com/example/demo/service/ReservationService.java
@@ -35,12 +35,13 @@ public class ReservationService {
     }
 
     // TODO: 1. 트랜잭션 이해
+    @Transactional
     public void createReservation(Long itemId, Long userId, LocalDateTime startAt, LocalDateTime endAt) {
         // 쉽게 데이터를 생성하려면 아래 유효성검사 주석 처리
-        List<Reservation> haveReservations = reservationRepository.findConflictingReservations(itemId, startAt, endAt);
-        if(!haveReservations.isEmpty()) {
-            throw new ReservationConflictException("해당 물건은 이미 그 시간에 예약이 있습니다.");
-        }
+//        List<Reservation> haveReservations = reservationRepository.findConflictingReservations(itemId, startAt, endAt);
+//        if(!haveReservations.isEmpty()) {
+//            throw new ReservationConflictException("해당 물건은 이미 그 시간에 예약이 있습니다.");
+//        }
 
         Item item = itemRepository.findById(itemId).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 값이 존재하지 않습니다."));
         User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 값이 존재하지 않습니다."));
