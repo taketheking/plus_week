3824cefd45947a590d6fdec778640e5d51aa86ca
taketheking <so4524@gmail.com>
2024-12-13 20:22:53 +0900

    Fix(function_2) : /admins 으로 시작하는 URL 에 모든 사용자가 접근할 수 있었던 문제를 ADMIN 권한을 가진 사용자만 접근할 수 있도록 수정
    Filter 대신 interceptor 를 이용하여 구현함

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.example.demo.config;
 import com.example.demo.entity.Role;
 import com.example.demo.filter.AuthFilter;
 import com.example.demo.filter.RoleFilter;
+import com.example.demo.interceptor.AdminRoleInterceptor;
 import com.example.demo.interceptor.AuthInterceptor;
 import com.example.demo.interceptor.UserRoleInterceptor;
 import jakarta.servlet.Filter;
@@ -21,15 +22,21 @@ public class WebConfig implements WebMvcConfigurer {
     // TODO: 2. 인가에 대한 이해
     private static final String[] AUTH_REQUIRED_PATH_PATTERNS = {"/users/logout", "/admins/*", "/items/*"};
     private static final String[] USER_ROLE_REQUIRED_PATH_PATTERNS = {"/reservations/*"};
+    private static final String[] ADMIN_ROLE_REQUIRED_PATH_PATTERNS = {"/admins/*"};
 
     private final AuthInterceptor authInterceptor;
     private final UserRoleInterceptor userRoleInterceptor;
+    private final AdminRoleInterceptor adminRoleInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor)
                 .addPathPatterns(AUTH_REQUIRED_PATH_PATTERNS)
                 .order(Ordered.HIGHEST_PRECEDENCE);
+
+        registry.addInterceptor(adminRoleInterceptor)
+                .addPathPatterns(ADMIN_ROLE_REQUIRED_PATH_PATTERNS)
+                .order(Ordered.HIGHEST_PRECEDENCE + 1);
 
         registry.addInterceptor(userRoleInterceptor)
                 .addPathPatterns(USER_ROLE_REQUIRED_PATH_PATTERNS)
@@ -44,6 +51,15 @@ public class WebConfig implements WebMvcConfigurer {
         filterRegistrationBean.addUrlPatterns(AUTH_REQUIRED_PATH_PATTERNS);
         return filterRegistrationBean;
     }
+
+//    @Bean
+//    public FilterRegistrationBean adminRoleFilter() {
+//        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+//        filterRegistrationBean.setFilter(new RoleFilter(Role.ADMIN));
+//        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE + 1);
+//        filterRegistrationBean.addUrlPatterns(ADMIN_ROLE_REQUIRED_PATH_PATTERNS);
+//        return filterRegistrationBean;
+//    }
 
     @Bean
     public FilterRegistrationBean userRoleFilter() {
